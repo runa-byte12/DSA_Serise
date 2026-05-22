ed841496a68f849ba43d79b924ef39c3fc100878
runanexus00-debug <runanexus.00@gmail.com>
2026-05-22 16:53:45 +0600

    Array

--- Array1.cpp
+++ Array1.cpp
@@ -4,14 +4,19 @@ int main(){
     //find smallest and largest number in array
     int num []={12,23,199,-1,-80,29};
     int size=7;
+    int largest=INT_MIN;
     int smallest=INT_MAX;
     for(int i=0;i<size;i++){
+       /* if(num[i]<smallest){
+            smallest=num[i];
+        }*/
 
-        if(num[i]<smallest){
-       smallest=num[i];
-        }
+    largest=max(num[i],largest);
+    smallest=min(num[i],smallest);
 
     }
-    cout<<"the smallest number="<<smallest;
+    cout<<"the smallest number="<<smallest<<endl;
+
+    cout<<"the largest number="<<largest;
     return 0;
 }
